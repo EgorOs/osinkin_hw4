dc613c63036f779ecb360f1d6efb1e4f68e3052f
Egor Osinkin <egoros97@yandex.ru>
2018-08-17 02:32:49 +0300

    Task_2 fixed, Task_5 finished

--- task_2.py
+++ task_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def mysqrt(x, decimal=0.1, precision=0.001):
+def mysqrt(x, precision=1):
     """
     Returns a square root of the integer or float number, if
     resulting float not bigger than sys.float_info.max, which is
@@ -14,7 +14,7 @@ def mysqrt(x, decimal=0.1, precision=0.001):
     precision - float, that defines the error toleration
     """
 
-    def next_guess(x, guess, precision=0.001):
+    def next_guess(x, guess, precision=1, acc = 1):
         new_guess = (guess + x / guess) / 2
         error = abs(new_guess - guess)
         if error < precision:
@@ -24,15 +24,17 @@ def mysqrt(x, decimal=0.1, precision=0.001):
 
     if x < 0: raise ValueError('{} is not positive number'.format(x))
     if x == 0: return 0
-    if x == 1: return 1
 
-    decimal_str = str(decimal)
+    decimal_str = str(precision)
     if 'e' in decimal_str:
         # for representation like 1e-11
         decimal = int(decimal_str.split('-')[1])
-    else:
+    elif '.' in decimal_str:
         decimal = len(decimal_str.split('.')[1])
-    return round(next_guess(x, x / 2, precision), ndigits=decimal)
+    else:
+        decimal = 1
+    return round(next_guess(x, x / 2, precision/10), ndigits=decimal)
 
 
-# print(mysqrt(4/1000, 0.0001))
+# print(mysqrt(4/1000000000, 0.0000000000001))
+# print((4/1000000000)**0.5)
--- task_5.py
+++ task_5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from time import time, sleep
+import math
 
 
 def parametrized(dec):
@@ -15,9 +16,9 @@ def parametrized(dec):
 @parametrized
 def profiler(function, calls_info):
     def wrapper(*args, **kwargs):
-        start_time = time()
+        start_time = time()*1000
         result = function(*args, **kwargs)
-        end_time = time()
+        end_time = time()*1000
         execution_time = end_time - start_time
 
         if globals().get(calls_info):
@@ -30,67 +31,70 @@ def profiler(function, calls_info):
     return wrapper
 
 
-# Factorial 1
-class Recurse(Exception):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+@profiler('test_gold')
+def golden_ratio( n ):
+    phi = (1 + math.sqrt( 5 ) ) / 2
+    psi = (1 - math.sqrt( 5 ) ) / 2
+    return round(( phi**n ) / math.sqrt( 5 ))
 
 
-def recurse(*args, **kwargs):
-    raise Recurse(*args, **kwargs)
-
-
-def tail_recursive(f):
-    def decorated(*args, **kwargs):
-        while True:
-            try:
-                return f(*args, **kwargs)
-            except Recurse as r:
-                args = r.args
-                kwargs = r.kwargs
-                continue
-
-    return decorated
-
-
-@profiler('F1_info')
-@tail_recursive
-def fact(n, acc=1):
-    if n == 1: return acc
-    return recurse(n - 1, n * acc)
-
-
-# Factorial 2
-@profiler('F2_info')
-def factorial(n):
-    if n < 0:
-        return
-    if n == 0:
+@profiler('test_rec')
+def fib_rec(n):
+    if n < 3:
         return 1
-    return n * factorial(n - 1)
+    else:
+        return fib_rec(n - 1) + fib_rec(n - 2)
 
 
-# Factorial 3
-@profiler('F3_info')
-def tail_factorial(n, acc=1):
-    if n < 0:
-        return
-    if n == 1:
-        return acc
-    return tail_factorial(n - 1, acc * n)
+@profiler('test_iter')
+def fib_iter(n):
+    a = 0
+    b = 1
+    for __ in range(n):
+        a, b = b, a + b
+    return a
 
 
-n = 200
-print('\nFactorial of', n)
-print('\nTail_recursive with decorator:')
-print(fact(n))
-print('Calls: {}, total time: {}'.format(*F1_info))
+@profiler('test_marix')
+def fib_matrix(n):
+    def my_pow(x, k, I, mult):
+        """
+        Возвращает x в степени n. Предполагает, что I – это единичная матрица, которая
+        перемножается с mult, а n – положительное целое
+        """
+        if k == 0:
+            return I
+        elif k == 1:
+            return x
+        else:
+            y = my_pow(x, k // 2, I, mult)
+            y = mult(y, y)
+            if k % 2:
+                y = mult(x, y)
+            return y
 
-print('\nUnoptimized:')
-print(factorial(n))
-print('Calls: {}, total time: {}'.format(*F2_info))
+    def identity_matrix(j):
+        """Возвращает единичную матрицу n на n"""
+        r = list(range(j))
+        return [[1 if i == j else 0 for i in r] for j in r]
 
-print('\nTail call optimization:')
-print(tail_factorial(n))
-print('Calls: {}, total time: {}'.format(*F3_info))
+    def matrix_multiply(A, B):
+        bt = list(zip(*B))
+        return [[
+            sum(a * b for a, b in zip(row_a, col_b))
+            for col_b in bt]
+            for row_a in A]
+    f = my_pow([[1, 1], [1, 0]], n, identity_matrix(2), matrix_multiply)
+    return f[0][1]
+
+
+n = 30
+
+print(fib_rec(n))
+print('Recursive\ncalls: {}\ntime(ms): {}'.format(*test_rec))
+print(fib_iter(n))
+print('Iterative\ncalls: {}\ntime(ms): {}'.format(*test_iter))
+print(fib_matrix(n))
+print('Matirx\ncalls: {}\ntime(ms): {}'.format(*test_marix))
+print(golden_ratio(n))
+print('Gold ratio\ncalls: {}\ntime(ms): {}'.format(*test_gold))

ac43c9b027beadab7b1d40ac6a113a67ac30e0b4
Egor Osinkin <egoros97@yandex.ru>
2018-08-30 00:08:28 +0300

    Fixes for exponential representation

--- task_2.py
+++ task_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-def mysqrt(x, precision=1):
+def mysqrt(x, precision=None):
     """
     Returns a square root of the integer or float number, if
     resulting float not bigger than sys.float_info.max, which is
@@ -25,16 +25,29 @@ def mysqrt(x, precision=1):
     if x < 0: raise ValueError('{} is not positive number'.format(x))
     if x == 0: return 0
 
+    # Representation by default
+    if precision is None:
+        if 'e' in str(x):
+            # find the power in exponential represenation 
+            precision = float('1e'+str(x).split('e')[1])
+        elif '.' in str(x):
+            # pre-defined high presicion for float input values
+            precision = 10 ** (-20)
+        else:
+            # calculate up to 1st decimal digit for integer input
+            precision = 1
+
+    # Representation with given precision parameter
     decimal_str = str(precision)
     if 'e' in decimal_str:
-        # for representation like 1e-11
         decimal = int(decimal_str.split('-')[1])
     elif '.' in decimal_str:
         decimal = len(decimal_str.split('.')[1])
     else:
         decimal = 1
+
     return round(next_guess(x, x / 2, precision/10), ndigits=decimal)
 
 
-# print(mysqrt(4/1000000000, 0.0000000000001))
-# print((4/1000000000)**0.5)
+# print(mysqrt(5e-256))
+# print((5e-256)**0.5)
